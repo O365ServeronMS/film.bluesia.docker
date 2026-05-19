a8831fc5d75c8d0ecead00d01dfd966c0a5a7304
O365ServeronMS <thangpd@office365server.onmicrosoft.com>
2026-05-19 13:39:15 +0700

    Ignore local cache and workspace files

--- scratch/add-git-path.ps1
+++
@@ -1,9 +0,0 @@
-$currentPath = [Environment]::GetEnvironmentVariable('PATH', 'User')
-$gitPath = 'C:\Program Files\Git\cmd'
-if ($currentPath -notmatch [regex]::Escape($gitPath)) {
-    $newPath = $currentPath + ';' + $gitPath
-    [Environment]::SetEnvironmentVariable('PATH', $newPath, 'User')
-    Write-Host 'Git has been added to the User PATH successfully.'
-} else {
-    Write-Host 'Git is already in the User PATH.'
-}
